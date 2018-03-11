22c6c2d1dd793c18d4b0fafcab0673a019964ede
KATO Kohaku <kato.satoshi.0@gmail.com>
2018-03-11 23:35:33 +0900

    add: plot

--- datagen.R
+++ datagen.R
@@ -8,8 +8,8 @@ n.major <- 80
 n.minor <- 20
 n.pop <- n.major + n.minor
 
-trend.down <- rnorm(n = 10^4, mean = -1)
-trend.up   <- rnorm(n = 10^4, mean = +1)
+trend.down <- rnorm(n = 10^4, mean = -0.3)
+trend.up   <- rnorm(n = 10^4, mean = +0.3)
 
 rbind(data.frame(trend="down", value = trend.down),
       data.frame(trend="up",   value = trend.up)) %>% 
@@ -30,21 +30,27 @@ p.negative <- npforeach(i = 1:n.pop, .combine = rbind)({
   dates <- sample(date_seq,   size = n.obs, replace = FALSE) %>% sort
   moment<- sample(samp, size = n.obs, replace = FALSE)
   
-  data.frame(date = dates, value = cumsum(moment), label = label)
+  data.frame(ID = i, date = dates, value = cumsum(moment), src = as.factor(label), class = "negative")
 })
 tail(p.negative)
 # population with positive trends
 p.positive <- npforeach(i = 1:n.pop, .combine = rbind)({
-  
-  samp <- trend.up
-  label <- "up"
-  if(i > n.major){
-    samp <- trend.down
-    label <- "down"
+
+  samp <- trend.down
+  label <- "down"
+  if(i > n.minor){
+    samp <- trend.up
+    label <- "up"
   }
   n.obs <- sample(min.measure:max.measure,1)
   dates <- sample(date_seq,   size = n.obs, replace = FALSE) %>% sort
   moment<- sample(samp, size = n.obs, replace = FALSE)
   
-  data.frame(date = dates, value = cumsum(moment), label = label)
+  data.frame(ID = i, date = dates, value = cumsum(moment), src = as.factor(label), class = "positive")
 })
+tail(p.positive)
+
+ggplot(data=p.positive, aes(x=date, y=value, group = factor(ID), colour=factor(src))) +
+  geom_line()
+ggplot(data=p.negative, aes(x=date, y=value, group = factor(ID), colour=factor(src))) +
+  geom_line()
